ecde8229e0aa719bccd5d371dbe06af5b599ac7e
ncsfanbros2001 <ncssuperfan@gmail.com>
2021-05-08 15:31:16 +0700

    submit assignment 1

---
+++ SQL/Assignment 1/Extra_Assignment_1_2.sql
@@ -0,0 +1,38 @@
+create database if not exists assignment1extra;
+use assignment1extra;
+--- Exercise 1
+create table Trainee
+(
+TraineeID int auto_increment primary key,
+Full_Name varchar(20),
+Birth_Date date,
+Gender enum('Male', 'Female', 'Unknown'),
+ET_IQ int check (ET_IQ > -1 and ET_IQ < 21),
+ET_Gmath int check (ET_Gmath > -1 and ET_Gmath < 21),
+ET_English int check (ET_English > -1 and ET_English < 51),
+Training_Class int,
+Evaluation_Notes text
+);
+alter table Trainee
+add VTI_Account varchar(20) not null unique;
+
+--- Exercise 2
+create table ExerciseTwo
+(
+ID mediumint auto_increment primary key,
+`Name` tinytext,
+`Code` varchar(5),
+ModifiedDate datetime
+);
+
+--- Exercise 3
+create table ExerciseThree
+(
+ID mediumint auto_increment primary key,
+`Name` tinytext,
+`Code` varchar(5),
+Gender bit,
+IsDeletedFlag enum('0', '1')
+);
+
+
---
+++ SQL/Assignment 1/Testing_System_Assignment_1_MySQL.sql
@@ -0,0 +1,96 @@
+create database if not exists Assignment1;
+use assignment1;
+--- TBL 1
+create table if not exists Department
+(
+DepartmentID int auto_increment primary key,
+DepartmentName varchar(20)
+);
+--- TBL 2
+create table if not exists `Position`
+(
+PositionID int auto_increment primary key,
+PositionName enum('Dev', 'Test', 'Scrum Master', 'PM')
+);
+--- TBL 3
+create table if not exists `Account`
+(
+AccountID int auto_increment primary key,
+Email varchar(30),
+Username varchar(20),
+Fullname varchar(20),
+DepartmentID int,
+PositionID int,
+foreign key (DepartmentID) references Department(DepartmentID),
+foreign key (PositionID) references `Position`(PositionID),
+CreateDate date
+);
+--- TBL 4
+create table if not exists `Group`
+(
+GroupID int primary key,
+GroupName varchar(20),
+CreatorID int,
+CreateDate date
+);
+--- TBL 5
+create table if not exists GroupAccount
+(
+GroupID int,
+AccountID int,
+Foreign key (GroupID) references `Group`(GroupID),
+Foreign key (AccountID) references Account(AccountID),
+JoinDate date
+);
+--- TBL 6
+create table if not exists TypeQuestion
+(
+TypeID int auto_increment primary key,
+TypeName enum('Essay', 'Multiple-Choice')
+);
+--- TBL 7
+create table if not exists CategoryQuestion
+(
+CategoryID int auto_increment primary key,
+CategoryName varchar(20)
+);
+--- TBL 8
+create table if not exists Question
+(
+QuestionID int auto_increment primary key,
+Content varchar(50),
+CategoryID int,
+TypeID int,
+Foreign key (CategoryID) references CategoryQuestion(CategoryID),
+Foreign key (TypeID) references TypeQuestion(TypeID),
+CreateDate date,
+CreatorID int
+);
+--- TBL 9
+create table if not exists Answer
+(
+AnswerId int auto_increment primary key,
+Content varchar(50),
+QuestionID int,
+isCorrect enum('True', 'False')
+);
+--- TBL 10
+create table if not exists Exam
+(
+ExamID int auto_increment primary key,
+`Code` int,
+Title varchar(30),
+CategoryID int,
+Foreign key (CategoryID) references CategoryQuestion(CategoryID),
+Duration time,
+CreateDate date,
+CreatorID int
+);
+--- TBL 11
+create table if not exists ExamQuestion
+(
+ExamID int,
+QuestionID int,
+Foreign key (ExamID) references Exam(ExamID),
+Foreign key (QuestionID) references Question(QuestionID)
+);
